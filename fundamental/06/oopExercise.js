2c9a6b596a4b68d9923c808a74d40059aec8c42c
dwiparay <dwiparay@gmail.com>
2023-09-04 20:11:21 +0700

    update

---
+++ fundamental/06/oopExercise.js
@@ -0,0 +1,51 @@
+class Product {
+    static productList = [];
+
+
+    constructor() {
+        this.name = "";
+        this.price = 0;
+    }
+
+    set setProductname(name) {
+        this.name = name;
+    }
+
+    get getProductname() {
+        return this.name;
+    }
+
+    set setPrice(price){
+        this.price = price;
+    }
+}
+
+class Transaction {
+    constructor() {
+        this.total = 0;
+        this.productCart = [];
+    }
+
+    addtoCart(productObj, qty) {}
+
+    showTotal() {
+        return this.total;
+    }
+
+    checkoutMethod() {}
+};
+
+let product1 = new Product()
+product1.setProductname = "Mouse Logitech B100";
+product1.setPrice = 90000;
+
+let product2 = new Product()
+product2.setProductname = "Mouse Logitech K100";
+product2.setPrice = 100000;
+
+console.log(product1);
+console.log(product2);
+
+Product.productList.push(product1, product2);
+
+console.log(Product.productList);
